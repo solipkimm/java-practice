aff10890e101636c2f3865cc484c3e67981328dc
Solip Kim <59662367+solipkimm@users.noreply.github.com>
2021-02-14 03:36:53 -0500

    Update CollegeStudent.java
    add error sign

--- demo-code/week05/Week5_1/CollegeStudent.java
+++ demo-code/week05/Week5_1/CollegeStudent.java
@@ -3,5 +3,6 @@ public class CollegeStudent extends Student {
 	
 	public static void main(String[] args) {
 		CollegeStudent collegeStudent1 = new CollegeStudent();
+		// -> error because there is no default constructor in Student
 	}
 }
